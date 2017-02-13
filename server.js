ef99d29008898aa9c8c0dc3873e6677debc8fa01
Thomas Danner <thmsdnnr@gmail.com>
2017-02-13 17:00:59 -0600

    adding auth check for secured routes

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(passport.session());
 app.use(flash());
 
 app.use(express.static(path.join(__dirname+'/static')));
-app.use(['/login','/register','/mod','/new','/b','/s'],bodyParser.urlencoded({extended:true}));
+app.use(['/login','/register','/new','/s'],bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname+'/views'));
 
@@ -66,13 +66,13 @@ function auth(req,res,next) { //req.sessionStore req.sessionID req.session
  app.get('/register', (req,res) => {
    res.render('register',{data:'',user:req.session.user,message:req.flash('registerMessage')});
  });
- /* --- AUTHENTICATION: Login & Registration --- */
 
  app.get('/logout', function (req,res) {
    req.logout();
    req.session.destroy();
    res.redirect('/login');
  });
+ /* --- END OF AUTHENTICATION: Login & Registration --- */
 
 app.post('/new', function(req,res) {
   let r=req.body;
@@ -80,44 +80,16 @@ app.post('/new', function(req,res) {
   serverObj.meta={title:r.title, author:r.author, description: r.description, size: r.size, dateCreated:new Date(),
     stats:{totalSolves:0, averageSolveTime:0}};
   serverObj.data=r.tsv;
-
   Board.findOrCreate(serverObj, function(err, data){
     if (!err) {
     res.redirect(`/b/${data._id}`);}
   });
 });
 
-app.get('/new', function(req,res) {
-  let d=new Date();
+app.get('/new', auth, function(req,res) {
   let payload={data:'', user:req.session.user};
   res.render('newBoard', {data:payload});
 });
-
-function fetchBoard(title) { //Returns a promise to board data when given EITHER of an Object ID primary key or a board Title
-  //http://stackoverflow.com/a/29231016
-  //test: if title casted to ObjectID matches title from object ID, we've been given an object ID.
-  let testObj;
-  try  { testObj = new ObjectId(title); }
-  catch(err) { } //it's not a valid object ID
-  if (testObj&&testObj.toString()===title) { //then this is a 24-character ObjectID corresponding to the primary key
-    return new Promise(function(resolve, reject) {
-      Board.findById(title,function(err,data) {
-        if (err) { reject(err); }
-        resolve(data);
-      });
-    });
-  }
-  else {
-  return new Promise(function(resolve, reject) {
-    //TODO handle conflicts of boards with the same title
-    //findOne for multiple results returns the most recent match ordered by insertion time
-    Board.findByTitle(title,function(err,data) {
-      if (err) { reject(err); }
-      resolve(data);
-    });
-  });
-  }
-}
 
 app.get('/b/:board', function(req,res) {
   let board = fetchBoard(req.params.board);
@@ -136,7 +108,7 @@ app.get('/boardlist', function(req,res) { //lists boards with links to them
   });
 });
 
-app.get('/mine', function(req,res) {
+app.get('/mine', auth, function(req,res) {
   Board.findBoardsByUser(req.session.user, function(err,data) {
     if(!err) {
       if (data) {
@@ -153,8 +125,6 @@ app.get('/mine', function(req,res) {
 app.post('/s', function(req,res) {
   let b=req.body;
   Board.saveSolveData({id:b.id,totalSolves:b.totalSolves,averageSolveTime:b.averageSolveTime}, function(err,data) {
-
-
   });
   Meta.saveSolveData({id:b.id, user:req.session.user, solveData:b.timeData}, function(err, data) {
     if (!err) {
@@ -165,7 +135,7 @@ app.post('/s', function(req,res) {
   });
 });
 
-app.get('/meta', function(req,res) {
+app.get('/meta', auth, function(req,res) {
   User.fetchMetadata({username:req.session.user}, function(err, data) {
     let payload={data:data, user:req.session.user};
     res.render('mySolves',{data:payload});
@@ -175,5 +145,31 @@ app.get('/meta', function(req,res) {
  app.get('*', function(req,res) { //catch-all route
    res.redirect('/login');
  });
+
+ function fetchBoard(title) { //Returns a promise to board data when given EITHER of an Object ID primary key or a board Title
+   //http://stackoverflow.com/a/29231016
+   //test: if title casted to ObjectID matches title from object ID, we've been given an object ID.
+   let testObj;
+   try  { testObj = new ObjectId(title); }
+   catch(err) { } //it's not a valid object ID
+   if (testObj&&testObj.toString()===title) { //then this is a 24-character ObjectID corresponding to the primary key
+     return new Promise(function(resolve, reject) {
+       Board.findById(title,function(err,data) {
+         if (err) { reject(err); }
+         resolve(data);
+       });
+     });
+   }
+   else {
+   return new Promise(function(resolve, reject) {
+     //TODO handle conflicts of boards with the same title
+     //findOne for multiple results returns the most recent match ordered by insertion time
+     Board.findByTitle(title,function(err,data) {
+       if (err) { reject(err); }
+       resolve(data);
+     });
+   });
+   }
+ }
 
 app.listen(process.env.PORT||3000);
