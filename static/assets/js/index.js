f3e26d8d846ad0d4490ecd1a664315d91cf2526a
Kevin Bertram <kbrtrm@gmail.com>
2017-02-19 14:06:30 -0600

    Added logic for across and down lists

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -9,26 +9,6 @@ function boardsize() {
   $(".board").css('padding-bottom', boardSize);
 };
 
-$(document).ready(function() {
-  fontsize();
-  boardsize();
-  var clueNo = 0;
-
-  $('.cell:not(.cell.cell--filled)').each(function(i) {
-    var icol = $(this).index() + 1;
-    if ($(this).prev().children('.cell--text').length < 1 ||
-      $(this).parent().prev('.row').children('.cell:nth-child(' + icol + ')').children('.cell--text').length < 1
-    ) {
-      clueNo++;
-      $(this).append("<span class='clue-number'>" + clueNo + "</span>");
-      $(this).children('.clue-number').click(function() {
-        $(this).prev('.cell--text').focus();
-      });
-    }
-  });
-  $('.board').css('opacity', 1);
-});
-
 $(window).resize(function() {
   fontsize();
   boardsize();
@@ -43,27 +23,56 @@ function nextCellHasClass(currCell, elClass) {
   nextCell = currCell.parent().next();
   if (nextCell.hasClass(elClass)) {
     return true;
+  } else {
+    return false;
+  };
+}
+
+function prevCellHasClass(currCell, elClass) {
+  prevCell = currCell.parent().prev();
+  if (prevCell.hasClass(elClass)) {
+    return true;
+  } else {
+    return false;
   };
 }
 
 $('div#board').keydown(function(e) {
   var currentCell = $(document.activeElement);
   switch (e.which) {
+
     case 37: // left
-      currentCell.parent().prev().children('input')
-        .focus().putCursorAtEnd();
-      break;
+      if (prevCellHasClass(currentCell, 'cell--filled')) {
+        currentCell.parent().prevUntil(':not(.cell--filled)').prev().children('input')
+          .focus().putCursorAtEnd();
+      } else {
+        currentCell.parent().prev().children('input')
+          .focus().putCursorAtEnd();
+      }
+    break;
 
     case 38: // up
-      var col = currentCell.parent().index() + 1;
-      currentCell.parent().parent().prev()
-        .find('.cell:nth-child(' + col + ')')
-        .children('input').focus().putCursorAtEnd();
+      var col = currentCell.parent().data('col');
+      var row = currentCell.parent().data('row');
+      var prow = row-1;
+      function moveUpUntilTextCell(cr) {
+        if ($(".cell[data-col='" + col + "'][data-row='" + cr + "']").hasClass('cell--filled')) {
+          next = cr-1;
+          console.log(cr, next);
+          moveUpUntilTextCell(next);
+        } else {
+          $(".cell[data-col='" + col + "'][data-row='" + cr + "']").children('input')
+            .focus().putCursorAtEnd();
+        }
+      }
+      moveUpUntilTextCell(prow);
+
       break;
 
     case 39: // right
+
       if (nextCellHasClass(currentCell, 'cell--filled')) {
-        currentCell.parent().next().next().children('input')
+        currentCell.parent().nextUntil(':not(.cell--filled)').next().children('input')
           .focus().putCursorAtEnd();
       } else {
         currentCell.parent().next().children('input')
@@ -73,25 +82,70 @@ $('div#board').keydown(function(e) {
       break;
 
     case 40: // down
-      var col = currentCell.parent().index() + 1;
-      currentCell.parent().parent().next()
-        .find('.cell:nth-child(' + col + ')')
-        .children('input').focus().putCursorAtEnd();
+      var col = currentCell.parent().data('col');
+      var row = currentCell.parent().data('row');
+      var prow = row+1;
+      function moveDownUntilTextCell(cr) {
+        if ($(".cell[data-col='" + col + "'][data-row='" + cr + "']").hasClass('cell--filled')) {
+          nextd = cr+1;
+          console.log(cr, nextd);
+          moveDownUntilTextCell(nextd);
+        } else {
+          $(".cell[data-col='" + col + "'][data-row='" + cr + "']").children('input')
+            .focus().putCursorAtEnd();
+        }
+      }
+      moveDownUntilTextCell(prow);
       break;
 
     case 8:
       currentCell.val('').parent().prev().children('input')
-        .focus().putCursorAtEnd();
-
+        .focus().putCursorAtEnd
       break;
 
+    case 65:
+    case 66:
+    case 67:
+    case 68:
+    case 69:
+    case 70:
+    case 71:
+    case 72:
+    case 73:
+    case 74:
+    case 75:
+    case 76:
+    case 77:
+    case 78:
+    case 79:
+    case 80:
+    case 81:
+    case 82:
+    case 83:
+    case 84:
+    case 85:
+    case 86:
+    case 87:
+    case 88:
+    case 89:
+    case 90:
+      if (nextCellHasClass(currentCell, 'cell--filled')) {
+        currentCell.val(e.key).parent().nextUntil(':not(.cell--filled)').next().children('input')
+          .focus().putCursorAtEnd();
+      } else {
+        currentCell.val(e.key).parent().next().children('input')
+          .focus().putCursorAtEnd();
+      }
+
+        break;
+
     default:
-      currentCell.val(e.key).parent().next().children('input')
-        .focus().putCursorAtEnd();
+      break;
 
       //return; // exit this handler for other keys
   }
   e.preventDefault(); // prevent the default action (scroll / move caret)
+
 });
 
 jQuery.fn.putCursorAtEnd = function() {
@@ -133,3 +187,24 @@ jQuery.fn.putCursorAtEnd = function() {
   });
 
 };
+
+$(document).ready(function() {
+  fontsize();
+  boardsize();
+
+  var clueNo = 0;
+
+  $('.cell:not(.cell.cell--filled)').each(function(i) {
+    var icol = $(this).index() + 1;
+    if ($(this).prev().children('.cell--text').length < 1 ||
+      $(this).parent().prev('.row').children('.cell:nth-child(' + icol + ')').children('.cell--text').length < 1
+    ) {
+      clueNo++;
+      $(this).append("<span class='clue-number'>" + clueNo + "</span>");
+      $(this).children('.clue-number').click(function() {
+        $(this).prev('.cell--text').focus();
+      });
+    }
+  });
+  $('.board').css('opacity', 1);
+});
