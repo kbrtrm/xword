a32b90ec92f2511f9d8b8d5f46512dd4cf0f9c01
Kevin Bertram <kbrtrm@gmail.com>
2017-02-28 15:08:43 -0600

    grey vertical context highlight

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -188,6 +188,23 @@ jQuery.fn.putCursorAtEnd = function() {
 
 };
 
+$( ".cell--text" )
+  .focus(function () {
+    var curH = $(this).parent().data('hpk');
+    var curV = $(this).parent().data('vpk');
+    console.log(curH);
+    $('.cell--text').parent('.cell[data-hpk='+curH+']').children('.cell--text').css({
+      'background-color': '#d4edf7'
+    });
+    $('.cell--text').parent('.cell[data-vpk='+curV+']').children('.cell--text').css({
+      'background-color': '#eee'
+    });
+  }).blur(function(){
+    $('.cell--text').css({
+      'background-color': '#fff'
+    });
+  });
+
   fontsize();
   boardsize();
 
